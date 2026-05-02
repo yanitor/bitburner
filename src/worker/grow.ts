a00646e32b4eedbb12457adbc380a9566c88b69c
Yan <y.paiha@gmail.com>
2026-05-02 18:26:56 +0200

    grow script

---
+++ src/worker/grow.ts
@@ -0,0 +1,13 @@
+import { NS } from '@ns';
+
+export async function main(ns: NS): Promise<void> {
+  const hostname = ns.args[0] as string;
+
+  if (!hostname) {
+    return;
+  }
+
+  while (true) {
+    await ns.grow(hostname);
+  }
+}
